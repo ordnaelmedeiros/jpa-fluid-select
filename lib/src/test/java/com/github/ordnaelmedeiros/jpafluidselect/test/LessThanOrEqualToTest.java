906f346056599429b2f7a945f30d95121b1cbf6c
Matheus Mohr <mohr023@gmail.com>
2019-02-22 20:56:26 -0300

    Fixed le() predicate

---
+++ lib/src/test/java/com/github/ordnaelmedeiros/jpafluidselect/test/LessThanOrEqualToTest.java
@@ -0,0 +1,32 @@
+package com.github.ordnaelmedeiros.jpafluidselect.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import javax.persistence.NoResultException;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import com.github.ordnaelmedeiros.jpafluidselect.FSelect;
+import com.github.ordnaelmedeiros.jpafluidselect.base.SelectTestBase;
+import com.github.ordnaelmedeiros.jpafluidselect.models.People;
+import com.github.ordnaelmedeiros.jpafluidselect.models.People_;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class LessThanOrEqualToTest extends SelectTestBase {
+	
+	public void deveTrazerDoisRegistros() {
+		
+		List<People> resultList = new FSelect(em)
+			.from(People.class)
+			.where()
+				.le(People_.id, 2l)
+			.getResultList();
+		
+		assertEquals(2, resultList.size());
+	}
+}
+
